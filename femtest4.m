9d4c952d93f3d8e54d63d43079eb559feec694af
kieran-sllinux <newman.kieranw@gmail.com>
2015-07-02 10:39:18 +0100

    sorted singular M, now singular Ax, Ay

--- femtest4.m
+++ femtest4.m
@@ -1,19 +1,19 @@
 clear all
-poly1=[3,4,0,100,100,0,0,0,10,10];
+poly1=[3;4;0;100;100;0;0;0;10;10];
 d1=decsg(poly1);
-[p,e,t]=initmesh(d1);
-Ax=zeros(size(t,2),size(t,2));
-Ay=zeros(size(t,2),size(t,2));
-M=zeros(size(t,2),size(t,2));
-Nt=100;
+[p,e,t]=initmesh(d1,'hmax',50);
+Ax=zeros(size(p,2),size(p,2));
+Ay=zeros(size(p,2),size(p,2));
+M=zeros(size(p,2),size(p,2));
+Nt=10;
 Uriver=1;
-U=Uriver*ones(size(t,2),1);
-V=zeros(size(t,2),1);
-h=zeros(size(t,2),1);
+U=Uriver*ones(size(p,2),1);
+V=zeros(size(p,2),1);
+h=zeros(size(p,2),1);
 tau=1;
-ustore=NaN(size(t,2),Nt);
-vstore=NaN(size(t,2),Nt);
-hstore=NaN(size(t,2),Nt);
+ustore=NaN(size(p,2),Nt);
+vstore=NaN(size(p,2),Nt);
+hstore=NaN(size(p,2),Nt);
 ustore(:,1)=U;
 vstore(:,1)=V;
 hstore(:,1)=h;
@@ -22,6 +22,8 @@ for alpha=1:3
 i=t(alpha,k);
 for beta=1:3
 j=t(beta,k);
+i
+j
 M(i,j)=M(i,j)+mhat(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
 Ax(i,j)=Ax(i,j)+ahatx(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
 Ay(i,j)=Ay(i,j)+ahaty(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
@@ -30,9 +32,9 @@ end
 end
 Minv=inv(M);
 for time=2:Nt
-	   Gu=zeros(size(t,2),1);
-Gv=zeros(size(t,2),1);
-Gh=zeros(size(t,2),1);
+	   Gu=zeros(size(p,2),1);
+Gv=zeros(size(p,2),1);
+Gh=zeros(size(p,2),1);
 for k=1:size(t,2)
 for alpha=1:3
 i=t(alpha,k);
@@ -44,7 +46,7 @@ end
 AA=M-(tau^2)*(Ax*Minv*Ax+Ay*Minv*Ay);
 BB=-tau*(Ax*Minv*Gu-Ay*Minv*Gv);
 BBB=Gh-BB;
-hn=AA\BBB;
+hn=AA\BBB
 BBu=-tau*Ax*Minv*Ax*hn+Ax*Minv*Gu;
 BBv=-tau*Ay*Minv*Ay*hn+Ay*Minv*Gv;
 un=Ax\BBu;
