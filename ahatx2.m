c60771b588c59dc55028363ac3183573a48c0559
kieran-sllinux <newman.kieranw@gmail.com>
2015-07-08 17:56:51 +0100

    fixed boundary conditions

--- ahatx2.m
+++ ahatx2.m
@@ -1,4 +1,4 @@
-function out=Ahatx2(alpha,beta,x0,y0,x1,y1,x2,y2)
+function out=ahatx2(alpha,beta,x0,y0,x1,y1,x2,y2)
 d=[-1,1,0;-1,0,1];
 c=[1,-1,-1;0,1,0;0,0,1];
 detJ=((y2-y0)*(x1-x0))-((y1-y0)*(x2-x0));
@@ -7,7 +7,7 @@ lam=c(alpha,1)+c(alpha,2)*p+c(alpha,3)*q;
 lambda=[lam;0];
 JT=[x1-x0,y1-y0;x2-x0,y2-y0];
 D=[d(1,beta);d(2,beta)];
-hout=lambda.*(inv(JT)*D)*abs(detJ);
+hout=dot(lambda,(inv(JT)*D))*abs(detJ);
 end
 out=(1/6)*(hf(0.5,0)+hf(0,0.5)+hf(0.5,0.5));
 end
--- femtest4_6.m
+++ femtest4_6.m
@@ -1,9 +1,10 @@
 clear all
-xbpts=[0;200;200;0];
-ybpts=[-50;-50;50;50];
+xbpts=[0;1000;1000;0];
+ybpts=[-10;-10;10;10];
 poly1=[3;4;xbpts;ybpts];
 d1=decsg(poly1);
 [p,e,t]=initmesh(d1);
+[p,e,t]=refinemesh(d1,p,e,t);
 [p,e,t]=refinemesh(d1,p,e,t);
 [in,on]=inpolygon(p(1,:),p(2,:),xbpts,ybpts);
 xa=min(xbpts);
@@ -34,6 +35,10 @@ end
 end
 end
 Nn=sum(in);
+Nl=sum(left);
+Nr=sum(right);
+Ntop=sum(top);
+Nb=sum(bottom);
 NNodes=sum(in)-sum(on);
 Ax=zeros(Nn);
 Ay=zeros(Nn);
@@ -84,7 +89,7 @@ end
 tnew(4,:)=1;
 enew([3,4,5,6,7],:)=e([3,4,5,6,7],:);
 for i=1:max(max(t))
-	h(i)=cos((2*pi*p(1,i))/(max(xbpts)));
+	h(i)=cos((pi*p(1,i))/(max(xbpts)));
 	%U(i)=(2/max(xbpts))*pi*9.81*sin((2*pi*p(1,i))/(max(xbpts)));
  end
 %h=zeros(max(max(t)),1);
@@ -100,8 +105,8 @@ i=t(alpha,k);
 for beta=1:3
 j=t(beta,k);
 M(i,j)=M(i,j)+mhat(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
-Ax(i,j)=Ax(i,j)+ahatx(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
-Ay(i,j)=Ay(i,j)+ahaty(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
+Ax(i,j)=Ax(i,j)+ahatx2(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
+Ay(i,j)=Ay(i,j)+ahaty2(alpha,beta,p(1,t(1,k)),p(2,t(1,k)),p(1,t(2,k)),p(2,t(2,k)),p(1,t(3,k)),p(2,t(3,k)));
 end
 end
 
@@ -114,6 +119,7 @@ I=eye(length(U));
 K1=[Z,Z,I;Z,Z,Z;I,Z,Z];
 K2=[Z,Z,Z;Z,Z,I;Z,I,Z];
 for time=2:1+Nt/tau
+clear AAA G Gu Gv Gh
 	 Gu=zeros(size(p,2),1);
 Gv=zeros(size(p,2),1);
 Gh=zeros(size(p,2),1);
@@ -128,42 +134,65 @@ end
 G=[Gu;Gv;Gh];
 
 
-AAA=MM+tau*AAx*K1+tau*AAy*K2;
+AAA=MM-tau*AAx*K1-tau*AAy*K2;
 AAA2=AAA;
+r=1;
+s=1;
 for i=1:length(on)
 	if left(i)==1
 	for j=1:length(PSI)/3
-G(i)=G(i)-AAA(i,j)*Ulbound(i);
-AAA(i,j)=0;
-AAA(j,i)=0;
+G(j)=G(j)-AAA(i,j)*Ulbound(i);
+G(i)=Ulbound(i);
+%G(i)=AAA(i,j)*Ulbound(i);
 end
+AAA(:,i)=0;
+AAA(i,:)=0;
 AAA(i,i)=1;
+remu(r)=i;
+r=r+1;
 end
 if right(i)==1
 	for j=1:length(PSI)/3
-G(i)=G(i)-AAA(i,j)*Urbound(i);
-AAA(i,j)=0;
-AAA(j,i)=0;
+G(j)=G(j)-AAA(i,j)*Urbound(i);
+G(i)=Urbound(i);
 end
+AAA(i,:)=0;
+AAA(:,i)=0;
 AAA(i,i)=1;
+remu(r)=i;
+r=r+1;
 end
 if top(i)==1
 	for j=1:length(PSI)/3
-G((length(PSI)/3)+i)=G((length(PSI)/3)+i)-AAA((length(PSI)/3)+i,(length(PSI)/3)+j)*Vtbound(i);
+G((length(PSI)/3)+j)=G((length(PSI)/3)+j)-AAA((length(PSI)/3)+i,(length(PSI)/3)+j)*Vtbound(i);
+G((length(PSI)/3)+i)=Vtbound(i);
 AAA((length(PSI)/3)+i,(length(PSI)/3)+j)=0;
 AAA((length(PSI)/3)+j,(length(PSI)/3)+i)=0;
 end
-AAA(i,i)=1;
+AAA((length(PSI)/3)+i,:)=0;
+AAA(:,(length(PSI)/3)+i)=0;
+AAA((length(PSI)/3)+i,(length(PSI)/3)+i)=1;
+remv(s)=i;
+s=s+1;
 end
 if bottom(i)==1;
 for j=1:length(PSI)/3
-G((length(PSI)/3)+i)=G((length(PSI)/3)+i)-AAA((length(PSI)/3)+i,(length(PSI)/3)+j)*Vbbound(i);
+G((length(PSI)/3)+j)=G((length(PSI)/3)+j)-AAA((length(PSI)/3)+i,(length(PSI)/3)+j)*Vbbound(i);
+G((length(PSI)/3)+i)=Vbbound(i);
 AAA((length(PSI)/3)+i,(length(PSI)/3)+j)=0;
 AAA((length(PSI)/3)+j,(length(PSI)/3)+i)=0;
 end
-AAA(i,i)=1;
+AAA(:,i+length(PSI)/3)=0;
+AAA(i+length(PSI)/3)=0;
+AAA(i+length(PSI)/3,i+length(PSI)/3)=1;
+remv(s)=i;
+s=s+1;
 end
 end
+remv=remv+(length(PSI)/3);
+remove=[remu,remv];
+AAA2(remove,:)=[];
+AAA2(:,remove)=[];
 PSIn=AAA\G;
 psistore(:,time)=PSIn;
 U=PSIn(1:length(PSIn)/3);
@@ -182,27 +211,27 @@ end
 ustore=psistore(1:length(PSIn)/3,:);
 vstore=psistore(1+length(PSIn)/3:2*length(PSIn)/3,:);
 hstore=psistore(1+2*length(PSIn)/3:length(PSIn),:);
-symmAAA=zeros(size(AAA2));
-for i=2:size(AAA2,1)
-	for j=1:i
-		if issymmetric(AAA2(j:i,j:i))==1
-		symmAAA(j,i)=1;
-end
-end
-end
-symmAy=zeros(size(Ay));
-for i=2:size(Ay,1)
-        for j=1:i
-                if issymmetric(Ay(j:i,j:i))==1
-                symmAy(j,i)=1;
-end
-end
-end
-symmAx=zeros(size(Ax));
-for i=2:size(Ax,1)
-        for j=1:i
-                if issymmetric(Ax(j:i,j:i))==1
-                symmAx(j,i)=1;
-end
-end
-end
+%symmAAA=zeros(size(AAA2));
+%for i=2:size(AAA2,1)
+%	for j=1:i
+%		if issymmetric(AAA2(j:i,j:i))==1
+%		symmAAA(j,i)=1;
+%end
+%end
+%end
+%symmAy=zeros(size(Ay));
+%for i=2:size(Ay,1)
+%        for j=1:i
+%                if issymmetric(Ay(j:i,j:i))==1
+%                symmAy(j,i)=1;
+%end
+%end
+%end
+%symmAx=zeros(size(Ax));
+%for i=2:size(Ax,1)
+%        for j=1:i
+%                if issymmetric(Ax(j:i,j:i))==1
+%                symmAx(j,i)=1;
+%end
+%end
+%end
